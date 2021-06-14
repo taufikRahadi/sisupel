663a8ffb1cf3adec1c796252e6cf786c47f0088f
Ardi <oliviergiroud1998@gmail.com>
2021-06-14 10:19:57 +0700

    Include survey model for feature

--- src/application/survey-question/survey-question.module.ts
+++ src/application/survey-question/survey-question.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { SurveyQuestion, SurveyQuestionModel } from "src/model/survey-question.model";
+import { Survey, SurveyModel } from "src/model/survey.model";
 import { AuthenticationModule } from "../authentication/authentication.module";
 import { SurveyQuestionResolver } from "./survey-question.resolver";
 
@@ -11,6 +12,10 @@ import { SurveyQuestionResolver } from "./survey-question.resolver";
       {
         name: SurveyQuestion.name,
         schema: SurveyQuestionModel
+      },
+      {
+        name: Survey.name,
+        schema: SurveyModel
       }
     ])
   ],
